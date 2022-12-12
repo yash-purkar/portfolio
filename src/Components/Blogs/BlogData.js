867b4152eaae27af4a997af95ffb669573e9e80a
yashpurkar <yashpurkar7079@gmail.com>
2022-12-12 23:04:58 +0530

    removed blog

--- src/Components/Blogs/BlogData.js
+++ src/Components/Blogs/BlogData.js
@@ -1,4 +1,12 @@
 const data = [
+
+  {
+    heading: "Hoisting in JavaScript",
+    date: "Oct 2022",
+    text: "Hoisting is a phenomena in JavaScript, by which we can access the variables and functions even before initialization.",
+    cls: "blog_white",
+    blogUrl: "https://yashpurkar.hashnode.dev/hoisting-in-javascript-1"
+  },
   {
     heading: "undefined and not defined",
     date: "Nov 2022",
@@ -7,21 +15,6 @@ const data = [
     blogUrl: "https://yashpurkar.hashnode.dev/undefined-not-defined"
   },
 
-
-  {
-    heading: "Scope in JS",
-    date: "Oct 2022",
-    text: "Scope means where we can access a specific variable or a function in our code.",
-    cls: "blog_off_white",
-    blogUrl: "https://link.medium.com/w1n7Sid2Avb"
-  },
-  {
-    heading: "Hoisting in JavaScript",
-    date: "Oct 2022",
-    text: "Hoisting is a phenomena in JavaScript, by which we can access the variables and functions even before initialization.",
-    cls: "blog_white",
-    blogUrl: "https://yashpurkar.hashnode.dev/hoisting-in-javascript-1"
-  }
 ];
 
 export default data
