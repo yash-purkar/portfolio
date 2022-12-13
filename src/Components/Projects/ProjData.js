888c18a62fb39deb82f8ef73c081f7b8f87b2b72
yashpurkar <yashpurkar7079@gmail.com>
2022-12-13 12:45:19 +0530

    update project

--- src/Components/Projects/ProjData.js
+++ src/Components/Projects/ProjData.js
@@ -1,81 +1,31 @@
-
 const data = [
   {
-    heading: "Neog Bonus Work",
-    date: "December 2022",
-    text: "This App will check have you born in leap year or not?",
-    liveLink: "https://check-leap-year-or-not.netlify.app/",
-    code: "https://github.com/yash-purkar/Leap-Year"
+    heading: "Mark1: Do you know me really?",
+    date: "Octomber 2022",
+    text: "A CLI app built with NodeJs. You can send this to your friends and find out who really knows you.",
+    liveLink: "https://replit.com/@YashPurkar1/markOne?embed=1&output=1",
+    code: "https://github.com/yash-purkar/yashpurkar-markOne"
   },
   {
-    heading: "Mark15: Blogs",
-    date: "December 2022",
-    text: "Read My Blogs Here.",
-    btnNo: 1
+    heading: "Mark2: Are You Cricket Fan?",
+    date: "Octomber 2022",
+    text: "A CLI app built with NodeJs.You can send this to friend to check them interest in cricket.",
+    liveLink: "https://replit.com/@YashPurkar1/markTwo?embed=1&output=1",
+    code: "https://github.com/yash-purkar/yashpurkar-markTwo"
   },
   {
-    heading: "Mark14: Stocks Profit OR Loss",
-    date: "December 2022",
-    text: "Profit or loss app made using Vanilla JS, to check are you in profit or loss.",
-    liveLink: "https://stocks-profit-or-loss-calculator.netlify.app/",
-    code: "https://github.com/yash-purkar/stocks_profit_or_loss_calculator/blob/master/index.js"
-  },
-  {
-    heading: "Mark13: Is Your Birthday Palindrome",
-    date: "December 2022",
-    text: "Here you can check is your birthday palindrom or not.",
-    liveLink: "https://is-birthday-palindrome-or-not.netlify.app/",
-    code: "https://github.com/yash-purkar/isBirthdayPalindrome"
-  },
-  {
-    heading: "Mark12: Fun With Triangles",
-    date: "December 2022",
-    text: "Fun with a triangle, we can check is triangle is forming or not and also check hypoteneus of triangle.And We can also check area of triangle, and play a quiz about triangles",
-    liveLink: "https://yash-fun-with-triangle.netlify.app/",
-    code: "https://github.com/yash-purkar/funWithTriangles"
-  },
-  {
-    heading: "Mark11: Is Your Birthday Lucky?",
-    date: "December 2022",
-    text: "To check is your birthday lucky or not made using Vanilla JS.",
-    liveLink: "https://yash-purkar-is-your-birthday-lucky.netlify.app/",
-    code: "https://github.com/yash-purkar/IsYourBirthdayLucky"
-  },
-  {
-    heading: "Mark10: Cash Register Manager",
-    date: "December 2022",
-    text: "Cash Register App which helps the user by telling how can they return the change with minimum number of notes.",
-    liveLink: "https://yashpurkar-cash-register.netlify.app/",
-    code: "https://github.com/yash-purkar/cash-register"
-  },
-
-  {
-    heading: "Mark9: Movie Recommendation App",
-    date: "December 2022",
-    text: "Here I have share my favourite movies on particular topic.",
-    liveLink: "https://movie-recommondation.netlify.app/",
-    code: "https://github.com/yash-purkar/movie-recommondation"
-  },
-  {
-    heading: "Mark8: Emoji Interpreter",
-    date: "December 2022",
-    text: "Emoji Interpreter to check the meaning of Emoji.",
-    liveLink: "https://yash-emoji-interpreter.netlify.app",
-    code: "https://github.com/yash-purkar/emoji-interpreter"
-  },
-  {
-    heading: "Mark7: Jar Jar Binks Translator",
-    date: "December 2022",
-    text: "This is funny translator app , you can put your english sentence it will convert that into Jar Jar Binks.",
-    liveLink: "https://yash-jar-jar-binks.netlify.app/",
-    code: "https://github.com/yash-purkar/jar-jar-binks-translator"
-  },
-  {
-    heading: "Mark6: Banana Talk",
+    heading: "Mark3: CLI Github Link",
     date: "November 2022",
-    text: "Minion App to translate your text into minion, built with Vanilla JS",
-    liveLink: "https://yash-banana-translator.netlify.app/",
-    code: "https://github.com/yash-purkar/banana-translator"
+    text: "Here is the CLI project uploaded on github",
+    liveLink: "https://replit.com/@YashPurkar1/markTwo?embed=1&output=1",
+    code: "https://github.com/yash-purkar/yashpurkar-markTwo"
+  },
+  {
+    heading: "Mark4: Portfolio",
+    date: "November 2022",
+    text: "Here is the my portfolio with all my projects , some blogs and also with my social media links.",
+    liveLink: "https://yashpurkar-portfolio.netlify.app/",
+    code: "https://github.com/yash-purkar/portfolio"
   },
   {
     heading: "Mark5: Portfolio blogs page",
@@ -86,34 +36,82 @@ const data = [
     code: "https://github.com/yash-purkar/portfolio/tree/master/src/Components/Blogs"
   },
   {
-    heading: "Mark4: Portfolio",
+    heading: "Mark6: Banana Talk",
     date: "November 2022",
-    text: "Here is the my portfolio with all my projects , some blogs and also with my social media links.",
-    liveLink: "https://yashpurkar-portfolio.netlify.app/",
-    code: "https://github.com/yash-purkar/portfolio"
+    text: "Minion App to translate your text into minion, built with Vanilla JS",
+    liveLink: "https://yash-banana-translator.netlify.app/",
+    code: "https://github.com/yash-purkar/banana-translator"
   },
   {
-    heading: "Mark3: CLI Github Link",
-    date: "November 2022",
-    text: "Here is the CLI project uploaded on github",
-    liveLink: "https://replit.com/@YashPurkar1/markTwo?embed=1&output=1",
-    code: "https://github.com/yash-purkar/yashpurkar-markTwo"
+    heading: "Mark7: Jar Jar Binks Translator",
+    date: "December 2022",
+    text: "This is funny translator app , you can put your english sentence it will convert that into Jar Jar Binks.",
+    liveLink: "https://yash-jar-jar-binks.netlify.app/",
+    code: "https://github.com/yash-purkar/jar-jar-binks-translator"
+  },
+  {
+    heading: "Mark8: Emoji Interpreter",
+    date: "December 2022",
+    text: "Emoji Interpreter to check the meaning of Emoji.",
+    liveLink: "https://yash-emoji-interpreter.netlify.app",
+    code: "https://github.com/yash-purkar/emoji-interpreter"
+  },
+  {
+    heading: "Mark9: Movie Recommendation App",
+    date: "December 2022",
+    text: "Here I have share my favourite movies on particular topic.",
+    liveLink: "https://movie-recommondation.netlify.app/",
+    code: "https://github.com/yash-purkar/movie-recommondation"
+  },
+  {
+    heading: "Mark10: Cash Register Manager",
+    date: "December 2022",
+    text: "Cash Register App which helps the user by telling how can they return the change with minimum number of notes.",
+    liveLink: "https://yashpurkar-cash-register.netlify.app/",
+    code: "https://github.com/yash-purkar/cash-register"
+  },
+  {
+    heading: "Mark11: Is Your Birthday Lucky?",
+    date: "December 2022",
+    text: "To check is your birthday lucky or not made using Vanilla JS.",
+    liveLink: "https://yash-purkar-is-your-birthday-lucky.netlify.app/",
+    code: "https://github.com/yash-purkar/IsYourBirthdayLucky"
+  },
+  {
+    heading: "Mark12: Fun With Triangles",
+    date: "December 2022",
+    text: "Fun with a triangle, we can check is triangle is forming or not and also check hypoteneus of triangle.And We can also check area of triangle, and play a quiz about triangles",
+    liveLink: "https://yash-fun-with-triangle.netlify.app/",
+    code: "https://github.com/yash-purkar/funWithTriangles"
+  },
+  {
+    heading: "Mark13: Is Your Birthday Palindrome",
+    date: "December 2022",
+    text: "Here you can check is your birthday palindrom or not.",
+    liveLink: "https://is-birthday-palindrome-or-not.netlify.app/",
+    code: "https://github.com/yash-purkar/isBirthdayPalindrome"
+  },
+  {
+    heading: "Mark14: Stocks Profit OR Loss",
+    date: "December 2022",
+    text: "Profit or loss app made using Vanilla JS, to check are you in profit or loss.",
+    liveLink: "https://stocks-profit-or-loss-calculator.netlify.app/",
+    code: "https://github.com/yash-purkar/stocks_profit_or_loss_calculator/blob/master/index.js"
+  },
+  {
+    heading: "Mark15: Blogs",
+    date: "December 2022",
+    text: "Read My Blogs Here.",
+    btnNo: 1
+  },
+  {
+    heading: "Neog Bonus Work",
+    date: "December 2022",
+    text: "This App will check have you born in leap year or not?",
+    liveLink: "https://check-leap-year-or-not.netlify.app/",
+    code: "https://github.com/yash-purkar/Leap-Year"
   },
 
-  {
-    heading: "Mark2: Are You Cricket Fan?",
-    date: "Octomber 2022",
-    text: "A CLI app built with NodeJs.You can send this to friend to check them interest in cricket.",
-    liveLink: "https://replit.com/@YashPurkar1/markTwo?embed=1&output=1",
-    code: "https://github.com/yash-purkar/yashpurkar-markTwo"
-  },
-  {
-    heading: "Mark1: Do you know me really?",
-    date: "Octomber 2022",
-    text: "A CLI app built with NodeJs. You can send this to your friends and find out who really knows you.",
-    liveLink: "https://replit.com/@YashPurkar1/markOne?embed=1&output=1",
-    code: "https://github.com/yash-purkar/yashpurkar-markOne"
-  },
   {
     heading: "To-Do App",
     date: "November 2022",
