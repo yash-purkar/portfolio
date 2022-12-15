a02272734a5b8401c3fff32638c5ea18d94d179a
yashpurkar <yashpurkar7079@gmail.com>
2022-12-15 15:00:53 +0530

    docs: updated social links

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -9,11 +9,11 @@ const Footer = () => {
           <a href="https://github.com/yash-purkar" target="_blank" className="link" rel="noreferrer">Github</a>
         </li>
         <li className="list-item-inline">
-          <a href="https://twitter.com/_Yash_Purkar" target="_blank" className="link" rel="noreferrer">Twitter</a>
+          <a href="https://twitter.com/yashpurkar14" target="_blank" className="link" rel="noreferrer">Twitter</a>
         </li>
         <li className="list-item-inline">
           <a
-            href="https://www.linkedin.com/in/yash-purkar-a3505b235/"
+            href="https://www.linkedin.com/in/yash-purkar-32a67925a/"
             className="link" target="_blank"
             rel="noreferrer">Linkdin</a
           >
